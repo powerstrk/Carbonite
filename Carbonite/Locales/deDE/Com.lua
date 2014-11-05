c801d0feeb50ae440ba57423935b339980d7ea57
samyonair <admin@wowfriends.de>
2014-11-05 10:02:35 +0100

    Erste Schritte, First Steps
    Noch am Anfang 2 Dateien begonnen.
    
    At the Beginning, 2 Files edited but not finished

--- Carbonite/Locales/deDE/Com.lua
+++ Carbonite/Locales/deDE/Com.lua
@@ -5,21 +5,21 @@ end
 local L = LibStub("AceLocale-3.0"):NewLocale("Carbonite", "deDE", true)
 if not L then return end
 
-L["reached level"] = true
-L["Monitor Error: All 10 chat channels are in use"] = true
-L["This will disable some communication features"] = true
-L["You may free channels using the chat tab"] = true
+L["reached level"] = "erreichtes Level"
+L["Monitor Error: All 10 chat channels are in use"] = "Überwachungsfehler: Alle 10 Chat Channel sind in Benutzung"
+L["This will disable some communication features"] = "Dies wird einige Kommunikationsfunktionen deaktivieren"
+L["You may free channels using the chat tab"] = "Du kannst eventuell einige entfernen im Chat Menü"
 L["chat channel(s)!"] = true
 L["Need"] = true
-L["Monitored:"] = true
-L["Druid"] = true
-L["Hunter"] = true
-L["Mage"] = true
-L["Paladin"] = true
-L["Priest"] = true
-L["Rogue"] = true
-L["Shaman"] = true
-L["Warlock"] = true
-L["Warrior"] = true
-L["Deathknight"] = true
-L["Monk"] = true
+L["Monitored:"] = "überwacht:"
+L["Druid"] = "Druide"
+L["Hunter"] = "J\195\164ger"
+L["Mage"] = "Magier"
+L["Paladin"] = "Paladin"
+L["Priest"] = "Priester"
+L["Rogue"] = "Schurke"
+L["Shaman"] = "Schamane"
+L["Warlock"] = "Hexenmeister"
+L["Warrior"] = "Krieger"
+L["Deathknight"] = "Todesritter"
+L["Monk"] = "M\195\182nch"
