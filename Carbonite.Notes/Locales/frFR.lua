3844e49d0f62e779ec5658279e624c0d59be6add
Olivier <powerstrk@outlook.be>
2014-12-04 12:46:07 +0100

    Update frFR.lua
    adding translations

--- Carbonite.Notes/Locales/frFR.lua
+++ Carbonite.Notes/Locales/frFR.lua
@@ -1,4 +1,4 @@
-﻿if ( GetLocale() ~= "frFR" ) then
+if ( GetLocale() ~= "frFR" ) then
 	return;
 end
 
@@ -9,16 +9,16 @@ L["Note Options"] = "Options Note"
 L["Show Notes On Map"] = "Afficher Notes sur la Map"
 L["Shows your notes on the carbonite map"] = "Afficher vos Notes sur la Map Carbonite"
 L["Show Notes"] = "Afficher Notes"
-L["-Notes-"] = true
+L["-Notes-"] = "-Notes-"
 L["Add Note"] = "Ajouter Note"
-L["Notes Module"] = true
-L["Toggle Notes"] = true
+L["Notes Module"] = "Module Notes"
+L["Toggle Notes"] = "Basculer vers Notes"
 L["Record"] = "Enregistrer"
-L["Up"] = "Dessus"
-L["Down"] = "En Bas"
+L["Up"] = "Haut"
+L["Down"] = "Bas"
 L["Delete Item"] = "Effacer Objet"
 L["Name"] = "Nom"
-L["Type"] = true
+L["Type"] = "Type"
 L["Value"] = "Valeur"
 L["Location"] = "Emplacement"
 L["Select a favorite before recording"] = "S\195\169lectionner un favori avant l'enregistrement"
@@ -28,14 +28,14 @@ L["Rename"] = "Renommer"
 L["Cut"] = "Couper"
 L["Copy"] = "Copier"
 L["Paste"] = "Coller"
-L["Options"] = true
+L["Options"] = "Options"
 L["Add Comment"] = "Ajouter Commentaire"
 L["Set Icon"] = "D\195\169finir l'ic\195\180ne"
 L["Name"] = "Nom"
 L["Nothing to paste"] = "Rien \195\160 Coller"
 L["Can't paste that on the left side"] = "Impossible de coller ceci au c\195\160t\195\169 Gauche"
 L["Can't paste that on the right side"] = "Impossible de coller ceci au c\195\160t\195\169 Droit"
-L["Note"] = true
-L["Notes"] = true
-L["Note Addons"] = true
+L["Note"] = "Note"
+L["Notes"] = "Notes"
+L["Note Addons"] = "Notes add-on(s)"
 L["My Notes"] = "Mes Notes"
