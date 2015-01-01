9920faf23902148940f925fc0095aba684a06aa0
gOOvER <info@goover.de>
2015-01-01 09:17:34 +0100

    Small Fix for zhCN

--- Carbonite.Weekly/Locales/zhCN.lua
+++ Carbonite.Weekly/Locales/zhCN.lua
@@ -42,10 +42,10 @@ L["Not Completed"] = "未完成"
 -- MoP
 L["Sha of Anger"] = true
 L["Galleon"] = true
-L["Nalak, The Storm Lord"]
+L["Nalak, The Storm Lord"] = true
 L["Oondasta"] = true
 L["Celestials"] = true
-L["Ordos"]
+L["Ordos"] = true
 --WoD
 L["Rukhmar"] = true
 L["Drov the Ruiner"] = true
